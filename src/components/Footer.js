382c00ea42b771109715f251b6c90fdec6d7ea93
Birendra Kumar Pradhan <birendrapradhan112@gmail.com>
2024-11-25 00:07:52 +0530

    smale changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
             <>
                 <footer className="shadow-sm text-center text-lg-start text-light mt-4" style={{ backgroundColor: "rgb(8 47 73)" }}>
                     <div className="text-center p-3 shadow-sm" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
-                        © 2024 Copyright -
+                         2024©Copyright -
                         <a className="text-light text-decoration-none" href="/"> news24x7.com</a>
                     </div>
                 </footer>
